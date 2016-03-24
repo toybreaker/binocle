6c5f910076ca90c1324f3b756de2ae3879c68070
toybreaker <rokmabox@gmail.com>
2016-03-24 12:26:53 +0100

    claenup

--- gulpfile.js
+++ gulpfile.js
@@ -15,66 +15,52 @@ var imagemin     = require('gulp-imagemin');
 var pngquant     = require('imagemin-pngquant');
 var reload       = browserSync.reload;
 
-
-//TESTING JADE:
-// var jade = require('gulp-jade');
-//
-// gulp.task('templates', function() {
-//   var YOUR_LOCALS = {};
-//
-//   gulp.src('./src/*.jade')
-//     .pipe(jade({
-//       locals: YOUR_LOCALS
-//     }))
-//     .pipe(gulp.dest('./dist/'))
-// });
-
 // Reponsive sizing
 gulp.task('images', function () {
-  return gulp.src('./src/images/*.jpg')
+  return gulp.src('./_src/images/**/*')
     .pipe(responsive({
-    '*.jpg': [{
-        width:320,
+      '*.jpg': [{
+        width: 320,
         quality: 51,
         progressive: true,
         sharper: true,
         rename: {
-            suffix:'-320'
+          suffix: '-320'
         }
-    },{
-        width:640,
+      }, {
+        width: 640,
         quality: 51,
         progressive: true,
         sharper: true,
         rename: {
-            suffix:'-640'
+          suffix: '-640'
         }
-    },{
-        width:1024,
+      }, {
+        width: 1024,
         quality: 51,
         progressive: true,
         rename: {
-            suffix:'-1024'
+          suffix: '-1024'
         }
-    },{
-        width:1680,
+      }, {
+        width: 1680,
         quality: 71,
         progressive: true,
         rename: {
-            suffix:'-1680'
+          suffix: '-1680'
         }
-    }]
+      }]
     }))
     .pipe(gulp.dest('./assets/p'));
 });
 
 // Compress jpegs
 gulp.task('imagemin', function () {
-    return gulp.src('./assets/p/*.jpg')
-        .pipe(imagemin({
-            progressive: true
-        }))
-        .pipe(gulp.dest('./assets/p/min'));
+  return gulp.src('./assets/p/*.jpg')
+      pipe(imagemin({
+        progressive: true
+      }))
+      .pipe(gulp.dest('./assets/p/min'));
 });
 
 
@@ -82,32 +68,23 @@ gulp.task('imagemin', function () {
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
 
-    browserSync({
-        proxy: "roccomarosi.dev"
-    });
+  browserSync({
+    proxy: "binocle.dev"
+  });
 
-    gulp.watch("scss/*.scss", ['sass']);
-    gulp.watch("*.css").on('change', reload);
-    gulp.watch("*.htm").on('change', reload);
-    gulp.watch("*.js").on('change', reload);
+  gulp.watch("_sass/*.scss", ['sass']);
+  gulp.watch("*.css").on('change', reload);
+  gulp.watch("*.htm").on('change', reload);
+  gulp.watch("*.js").on('change', reload);
 });
 
 
 gulp.task('sass', function () {
-    return gulp.src('scss/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('assets/css'))// Write CSS (& Source maps)?
-        .pipe(filter('**/*.css')) // Filtering stream to only css files
-        .pipe(browserSync.reload({stream:true}));
+  return gulp.src('_sass/**/*.scss')
+      .pipe(sass())
+      .pipe(gulp.dest('assets/css'))// Write CSS (& Source maps)?
+      .pipe(filter('**/*.css')) // Filtering stream to only css files
+      .pipe(browserSync.reload({stream:true}));
 });
 
 gulp.task('default', ['serve']);
-
-
-// to autoprefix just use sublime (alt+super+p) > autoprefixer
-// it will prefix the whole css/scss file
-// https://github.com/sindresorhus/sublime-autoprefixer
-
-//todo:
-//minify task at least for css then maybe also html
-//package task to zip version while in dev
